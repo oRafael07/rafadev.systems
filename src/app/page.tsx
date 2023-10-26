5d89c4051f0fd743c172416e016e431012efeccd
Rafael Rocha <rafadeveloper.contato@gmail.com>
2023-10-26 10:26:34 -0400

    feat: soft skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import Slider from "@/components/slider";
 import { Button } from "@/components/ui/button";
-import { PhoneIcon } from "lucide-react";
+import { CodeIcon, PhoneIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -42,7 +43,7 @@ export default function Home() {
         <div className="mt-20">
           <h1 className="text-center text-xl font-bold">Sobre mim</h1>
 
-          <div className="mt-10 flex flex-col py-10 md:flex-row md:items-start md:gap-10 md:px-40">
+          <div className="mt-4 flex flex-col gap-10 py-10 md:flex-row md:items-start md:justify-between md:gap-10 md:px-40">
             <Image
               src="/me.jpg"
               alt="Eu"
@@ -54,15 +55,39 @@ export default function Home() {
               }}
               className="mx-auto h-auto max-h-[40%] w-full max-w-[40%] rounded-full md:max-h-[20%] md:max-w-[20%]"
             />
-            <p className="text-md px-4 py-8 text-center opacity-75 md:px-0 md:text-left">
-              Olá, meu nome é{" "}
-              <span className=" font-bold text-green-500 opacity-100">
-                Rafael!
-              </span>{" "}
-              tenho {Number(new Date().getFullYear() - 2004)} anos, sou
-              Desenvolvedor de Software e construo soluções escaláveis para seu
-              negócio.
-            </p>
+            <div className="px-10 md:px-0">
+              <h1 className="text-center text-2xl font-light md:text-left">
+                Olá, eu sou{" "}
+                <span className="font-bold text-green-500">Rafael</span>
+              </h1>
+              <p className="mt-4 text-sm text-zinc-400">
+                Trabalho com desenvolvimento de Software a 4 anos, construindo
+                soluções escaláveis e facilidade para negócio usando o melhor da
+                tecnologia.
+              </p>
+              <div className="flex justify-center md:justify-start">
+                <Button
+                  variant="outline"
+                  className="mt-4 justify-start gap-2 hover:bg-green-500"
+                >
+                  <CodeIcon size={16} />
+                  Soft skills
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="soft-skills">
+        <div className="mt-6">
+          <h1 className="text-center text-xl font-bold">Soft skills</h1>
+          <p className="mt-2 text-center text-sm text-zinc-400">
+            Veja um pouco das minhas experiências
+          </p>
+
+          <div className="my-20">
+            <Slider />
           </div>
         </div>
       </section>
---
+++ src/components/slider.tsx
@@ -0,0 +1,197 @@
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+
+export default function Slider() {
+  return (
+    <Marquee
+      pauseOnHover
+      autoFill
+      gradient
+      gradientColor="#000000"
+      gradientWidth={200}
+    >
+      <div className="w-[200px]">
+        <Image
+          src="/skills/postgres.svg"
+          alt="Postgresql"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[40%] w-full max-w-[40%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/docker.svg"
+          alt="Docker"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[60%] w-full max-w-[60%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/prisma.svg"
+          alt="Prisma"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[50%] w-full max-w-[50%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/nodejs.svg"
+          alt="NodeJS"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[30%] w-full max-w-[30%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/tailwindcss.svg"
+          alt="tailwindcss"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[50%] w-full max-w-[50%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/react.svg"
+          alt="React"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[20%] w-full max-w-[20%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/nextjs.svg"
+          alt="NextJS"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[40%] w-full max-w-[40%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/jest.svg"
+          alt="Jest"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[40%] w-full max-w-[40%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/github.svg"
+          alt="GitHub"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[30%] w-full max-w-[30%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/nginx.svg"
+          alt="Nginx"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[30%] w-full max-w-[30%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/ubuntu.svg"
+          alt="Ubuntu"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[30%] w-full max-w-[30%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/typescript.svg"
+          alt="Typescript"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[30%] w-full max-w-[30%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/cloudflare.svg"
+          alt="Cloudflare"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[50%] w-full max-w-[50%]"
+        />
+      </div>
+      <div className="w-[200px]">
+        <Image
+          src="/skills/digitalocean.svg"
+          alt="Cloudflare"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            objectFit: "contain",
+          }}
+          className="h-auto max-h-[70%] w-full max-w-[70%]"
+        />
+      </div>
+    </Marquee>
+  );
+}
