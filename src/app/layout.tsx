3df0e1f2debf0b37752237991335cb77a0493794
Rafael Rocha <rafadeveloper.contato@gmail.com>
2023-11-09 09:18:30 -0400

    feat: add analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/header";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "800"] });
 
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
