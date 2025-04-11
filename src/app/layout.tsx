423bd3fa2b55d4363ab4c8e4f8095073d75151cd
Rafael Rocha <rafadeveloper.contato@gmail.com>
2025-04-10 22:17:56 -0400

    feat: add clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          id="clarity-tracking"
+          dangerouslySetInnerHTML={{
+            __html: `
+             (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r297uq3j0x");
+            `,
+          }}
+        />
+      </head>
       <body className={poppins.className}>
         <Header />
         {children}
